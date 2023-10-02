d7cc779f420c735eb473601208860838b3bc989b
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-10-02 16:21:11 +0500

    Update product_item.dart

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -19,34 +19,38 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //final product = Provider.of<Product>(context);
-    return Consumer<Product>(
-      builder: (context, product, child) => ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTile(
-          footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            leading: IconButton(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: Consumer<Product>(
+            builder: (context, product, child) => IconButton(
               icon: Icon(
                 product.isFavorite ? Icons.favorite : Icons.favorite_border,
                 color: Theme.of(context).hintColor,
               ),
               onPressed: () => product.toggleIsFav(),
             ),
-            trailing: Consumer(
-              builder: (context, product, child) => IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.shopping_cart,
-                  color: Theme.of(context).hintColor,
-                ),
+          ),
+          trailing: Consumer(
+            builder: (context, product, child) => IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Theme.of(context).hintColor,
               ),
             ),
-            title: Text(
+          ),
+          title: Consumer<Product>(
+            builder: (context, product, child) => Text(
               product.title,
               textAlign: TextAlign.center,
             ),
           ),
-          child: GestureDetector(
+        ),
+        child: Consumer<Product>(
+          builder: (context, product, child) => GestureDetector(
             onTap: () {
               Navigator.of(context).pushNamed(
                 ProductDetailScreen.routeName,
