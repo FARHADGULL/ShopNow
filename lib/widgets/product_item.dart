2832637e3283393bb6e0165004680830160982c5
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-10-02 15:54:45 +0500

    Update product_item.dart

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -19,42 +19,44 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
-              color: Theme.of(context).hintColor,
-            ),
-            onPressed: () => product.toggleIsFav(),
-          ),
-          trailing: Consumer(
-            builder: (context, product, child) => IconButton(
-              onPressed: null,
+    return Consumer(
+      builder: (context, value, child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
               icon: Icon(
-                Icons.shopping_cart,
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
                 color: Theme.of(context).hintColor,
               ),
+              onPressed: () => product.toggleIsFav(),
+            ),
+            trailing: Consumer(
+              builder: (context, product, child) => IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.shopping_cart,
+                  color: Theme.of(context).hintColor,
+                ),
+              ),
+            ),
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
             ),
           ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-        ),
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName,
-              arguments: product.id,
-            );
-          },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                ProductDetailScreen.routeName,
+                arguments: product.id,
+              );
+            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       ),
