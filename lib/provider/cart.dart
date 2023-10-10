8ab127d4e6602fafd64c758a0a406b38615097c3
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-10-10 18:29:45 +0500

    Update cart.dart

--- lib/provider/cart.dart
+++ lib/provider/cart.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    required this.title,
+    required this.quantity,
+    required this.price,
+    required this.id,
+  });
+}
+
 class Cart with ChangeNotifier {}
