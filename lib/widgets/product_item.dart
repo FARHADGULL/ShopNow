fd5ce9a68554b08716c98660819fe013c273a0bc
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-10-11 22:11:32 +0500

    Update product_item.dart

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -52,7 +52,9 @@ class ProductItem extends StatelessWidget {
             ),
           ),
           trailing: IconButton(
-            onPressed: null,
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+            },
             icon: Icon(
               Icons.shopping_cart,
               color: Theme.of(context).hintColor,
