a6db6f91d147eb2aa73d4671b1edf9cbe4b316c8
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-10-13 11:39:31 +0500

    Create app_drawer.dart

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            title: Text('Hello Friend!'),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('Shop'),
+            onTap: (() {
+              Navigator.of(context).pushReplacementNamed('/');
+            }),
+          )
+        ],
+      ),
+    );
+  }
+}
