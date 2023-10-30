6a697c19c9bb14fb0f70528813d027e06543e109
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-10-30 11:55:25 +0500

    Update user_product_item.dart

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
 class UserProductItem extends StatelessWidget {
-  const UserProductItem({super.key});
+  final String title;
+  final String imageUrl;
+
+  const UserProductItem({
+    super.key,
+    required this.title,
+    required this.imageUrl,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: 100,
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(imageUrl),
+        ),
+        title: Text(title),
+        trailing: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.edit),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
