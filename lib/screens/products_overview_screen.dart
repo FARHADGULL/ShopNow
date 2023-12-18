2176c2cdc162dce52f4034a62c97f462ce30e253
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-12-18 15:55:39 +0500

    Update products_overview_screen.dart

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart' hide Badge;
 import 'package:provider/provider.dart';
 
+import '../provider/products.dart';
 import '../widgets/app_drawer.dart';
 import '../screens/cart_screen.dart';
 import '../provider/cart.dart';
@@ -22,6 +23,31 @@ class ProductsOverviewScreen extends StatefulWidget {
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavorites = false;
+  var _isInit = true;
+
+  @override
+  void initState() {
+    // Provider.of<Cart>(context, listen: false).fetchAndSetProducts(); // WON'T WORK! (context is not available)
+
+    // Future.delayed(Duration.zero).then((_) {
+    //   Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+    // }); // WILL WORK! (context is available) // This is a hack to get around the initState() method not having
+    //access to the context object. This is because the initState() method is executed before the widget is
+    //fully initialized. So, we use the Future.delayed() method to execute the code inside the then() method
+    //after the widget is fully initialized. This is a hack and not the best way to do this.
+    //A better way to do this is using didChangeDependencies() method.
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      Provider.of<Products>(context).fetchAndSetProducts();
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
